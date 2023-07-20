755361b8588b90d14c5017fc27dc94506d192fc2
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-07-20 23:31:42 +0600

    Update pacman.py

--- Project 1 - Pac Man/pacman.py
+++ Project 1 - Pac Man/pacman.py
@@ -1,8 +1,14 @@
 # import packages
 import pygame
+import math
 
 pygame.init()
 
 WIDTH = 900
 HEIGHT = 950
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+timer = pygame.font.Font('freesanbold.ttf', 20)
+level = boards
+color = 'blue'
+PI = math.pi
 
